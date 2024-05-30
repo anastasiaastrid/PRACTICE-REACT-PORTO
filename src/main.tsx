aebe0886a4a6fa63b6d7c87ffc8bc7b4e22cde4c
astridann <hasan.astrid@gmail.com>
2024-05-30 18:58:15 +0700

    update main.tsx 2

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
