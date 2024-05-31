bfe55879cd6a5fc0eb4bdf34286f4722a7935d48
astridann <hasan.astrid@gmail.com>
2024-05-31 14:23:50 +0700

    perubahan pada section about, perapihan margin

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -4,11 +4,11 @@ function About() {
       <h2 className="text-black text-[40px] font-bold"> Hi, im Astrid, a full stack web developer from Indonesia!</h2>
 
       <h2 className="my-5 text-black text-[14px]">
-        Hello, I'm Astrid. I'm a front end web developer, also a creative professional skilled in Graphic Design, Social
-        Media, 3D Modeling, and Motion Graphic Animation. With a passion for visual storytelling and a keen eye for
-        design aesthetics, I bring a versatile skill set to the table. Experienced in crafting engaging content across
-        various platforms, I thrive in the dynamic intersection of graphics, social media, and animation. Ready to
-        elevate your brand's visual presence and bring ideas to life with a touch of creativity.
+        Hello, I'm Astrid. I'm a full stack web developer, also a creative professional skilled in Graphic Design,
+        Social Media, 3D Modeling, and Motion Graphic Animation. With a passion for visual storytelling and a keen eye
+        for design aesthetics, I bring a versatile skill set to the table. Experienced in crafting engaging content
+        across various platforms, I thrive in the dynamic intersection of graphics, social media, and animation. Ready
+        to elevate your brand's visual presence and bring ideas to life with a touch of creativity.
       </h2>
     </div>
   );
