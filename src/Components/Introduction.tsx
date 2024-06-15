68c5404f0bcdf0553af23ae22175f103c77832c9
astridann <hasan.astrid@gmail.com>
2024-06-15 11:24:06 +0700

    revisi

--- src/Components/Introduction.tsx
+++ src/Components/Introduction.tsx
@@ -3,9 +3,7 @@ function Introduction() {
     <div className="flex justify-center flex-col items-center">
       <div className="h-[80px] border-r-[1px] mt-[0px]"></div>
 
-      <h2 className="mt-20 text-[15px] md:text-[20px] mt-5 text-black font-medium tracking-widest">
-        HELLO! MY NAME IS
-      </h2>
+      <h2 className="mt-20 text-[15px] md:text-[20px]  text-black font-medium tracking-widest">HELLO! MY NAME IS</h2>
       <h2 className="text-[30px] md:text-[70px] mt-2 text-black font-bold tracking-widest">ANASTASIA ASTRID</h2>
       <img className="bg-slate-300 p-4 w-[270px] h-[280px] rounded-full mt-7" src="Profileastrid.jpeg" />
     </div>
