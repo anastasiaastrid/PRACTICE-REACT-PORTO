6f86aeb489fd67bbb35fc0aad02cd9e7eed35d84
astridann <hasan.astrid@gmail.com>
2024-06-19 22:19:37 +0700

    ch

--- src/Components/Introduction.tsx
+++ src/Components/Introduction.tsx
@@ -2,8 +2,8 @@ function Introduction() {
   return (
     <div className="flex flex-col items-center w-full px-4">
       <div className="h-1 bg-gray-300 w-full max-w-screen-lg mx-auto my-10"></div>
-      <h2 className="mt-10 text-sm md:text-lg text-black font-medium tracking-widest">HELLO! MY NAME IS</h2>
-      <h2 className="text-4xl md:text-6xl mt-2 text-black font-bold tracking-widest">ANASTASIA ASTRID</h2>
+      <h2 className="text-center mt-10 text-sm md:text-lg text-black font-medium tracking-widest">HELLO! MY NAME IS</h2>
+      <h2 className="text-4xl md:text-6xl mt-2 text-center text-black font-bold tracking-widest">ANASTASIA ASTRID</h2>
       <div className="mt-7">
         <img className="w-52 h-40" src="CODE.jpg" alt="Profile" />
       </div>
